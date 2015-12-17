4d260c8c89387c16822381e0c3b3ebcc757be4ee
vidurbhatnagar <vidur.bhatnagar@gmail.com>
2015-12-16 21:36:47 -0500

    Vectorized TPS code from HW2

---
+++ code/est_tps.m
@@ -0,0 +1,33 @@
+function[a1,aX,aY,w] = est_tps(controlPts, targetValues)
+%|K  P||W| = |V|
+%|PT 0||ax|  |0|
+%      |ay|
+%      |a1| 
+rows = length(controlPts);
+
+% rMatrix = zeros(rows,rows);
+% for i = 1:rows
+%     for j = 1:rows
+%         rMatrix(i,j) = norm((controlPts(i,:)-controlPts(j,:)),2);
+%     end
+% end
+%rMatrixSq = rMatrix.^2;
+
+matDiff = @(cPts) repmat(cPts, 1, rows) - transpose(repmat(cPts, 1, rows));
+rMatrixSq = matDiff(controlPts(:,1)).^2 + matDiff(controlPts(:,2)).^2;
+kMatrix = rMatrixSq.*log(rMatrixSq) ;
+kMatrix(isnan(kMatrix)) = 0;
+pMatrix = [controlPts, ones(rows,1)];
+zeroMatrix  = zeros(3,3);
+concatMatrix = [kMatrix, pMatrix; pMatrix', zeroMatrix];
+
+lambda = eps;
+invConcatMatrix = inv(concatMatrix + lambda*eye(rows+3, rows+3));
+
+vVector = [targetValues; [0;0;0]];
+resultVector =  invConcatMatrix * vVector;
+
+w = resultVector(1:rows);
+aX = resultVector(rows+1);
+aY = resultVector(rows+2);
+a1 = resultVector(rows+3);
