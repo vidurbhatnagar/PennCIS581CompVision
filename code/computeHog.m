fbb441b2063c346f72de45cf31e327ced5740fbf
Prerna Srivastava <prerna.iiita@gmail.com>
2015-12-20 05:52:39 -0500

    Added comments to computeHog

--- code/computeHog.m
+++ code/computeHog.m
@@ -7,25 +7,29 @@ function [ H ] = computeHog( gradAngles, gradMags, numBins, signed )
 gradAngles = gradAngles(:);
 gradMags = gradMags(:);
 
+%% If not signed, then make negative angles positive
 binSize = pi/numBins;
 if ( signed==0 )
     gradAngles(gradAngles<0)=gradAngles(gradAngles<0) + pi;
 end
 
+%% Computing left and right bin indexes 
 leftBinIdx = round(gradAngles/binSize);
 rightBinIdx = leftBinIdx + 1;
 
 leftBinCen = ((leftBinIdx - 0.5)*binSize);
-%rightBinCen = ((rightBinIdx - 0.5)*binSize)
 
+%% Computing magnitude contributions to each bin based on the proportion of angles in each bin 
 leftBinProp = (gradAngles - leftBinCen);
 rightBinProp = (binSize - leftBinProp);
 leftBinProp = leftBinProp/binSize;
 rightBinProp = rightBinProp/binSize;
 
+%% Setting bins to wrap if they go out of range
 leftBinIdx(leftBinIdx==0) = numBins;
 rightBinIdx(rightBinIdx==(numBins+1)) = 1;
 
+%% Counting the angles in each bin according to their contributions
 H = zeros(numBins,1);
 for i=1:size(H,1)
     leftloc = find(leftBinIdx==i);
